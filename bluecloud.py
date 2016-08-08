ec2758acf39d99fe43bf4fcb2cb7a4b679ed363e
moe <moevery@gmail.com>
2016-08-09 00:17:23 +0800

    fix bug

--- bluecloud.py
+++ bluecloud.py
@@ -8,8 +8,7 @@ def getConfig():
     config.read('config.ini')
     identity = config.get('account', 'username')
     password = config.get('account', 'password')
-    
-    session = requests.Session()
+
     form = {
         'identity': identity,
         'password': password
@@ -17,17 +16,21 @@ def getConfig():
     return form
 
 def sendRequest(url, form):
+    session = requests.Session()
     response = session.post(url, data=form)
     tree = html.fromstring(response.content)
     segment = tree.xpath('//*[@id="home"]/div[3]/div[3]')[0]
     service_name = segment.xpath('//h4/text()')
     service_usage = segment.xpath('//div[@class="label"]/text()')
-        
+
     for name, usage in zip(service_name, service_usage):
         print name
         print usage
         print '--------------------------'
-    
+
 def main():
     form = getConfig()
-    sendRequest('https://bluecloud.xyz/auth/login', data)
+    sendRequest('https://bluecloud.xyz/auth/login', form)
+
+if __name__ == '__main__':
+    main()

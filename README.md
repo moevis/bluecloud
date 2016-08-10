86da7df94c4af18024c47e2de781712b3d5a4f87
moe <moevery@gmail.com>
2016-08-10 13:10:03 +0800

    auto generate config file at first time. progess the case when password or username incorrect.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 bluecloud 代理服务查询脚本
 
 在 config.ini 中配置好 username 和 password 后，运行 bluecloud.py 就可以了。
+也可以第一次运行时输入用户名和密码，会自动生成 config.ini
--- bluecloud.py
+++ bluecloud.py
@@ -1,28 +1,53 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ConfigParser
 import requests
 from lxml import html
+import os
 
 def getConfig():
     config = ConfigParser.ConfigParser()
-    config.read('config.ini')
-    identity = config.get('account', 'username')
-    password = config.get('account', 'password')
+    if os.path.isfile('config.ini'):
+        config.read('config.ini')
+        identity = config.get('account', 'username')
+        password = config.get('account', 'password')
 
+        form = {
+            'identity': identity,
+            'password': password
+        }
+    else:
+        form = createConfig()
+    return form
+
+def createConfig():
+    username = raw_input('username: ')
+    password = raw_input('password: ')
     form = {
-        'identity': identity,
+        'identity': username,
         'password': password
     }
+    configFile = open('config.ini', 'w')
+    config = ConfigParser.ConfigParser()
+    config.add_section('account')
+    config.set('account', 'username', username)
+    config.set('account', 'password', password)
+    config.write(configFile)
     return form
 
 def sendRequest(url, form):
     session = requests.Session()
     response = session.post(url, data=form)
     tree = html.fromstring(response.content)
-    segment = tree.xpath('//*[@id="home"]/div[3]/div[3]')[0]
-    service_name = segment.xpath('//h4/text()')
-    service_usage = segment.xpath('//div[@class="label"]/text()')
-
+    segment = tree.xpath('//*[@id="home"]/div[3]/div[3]')
+    if len(segment) > 0:
+        segment = segment[0]
+        service_name = segment.xpath('//h4/text()')
+        service_usage = segment.xpath('//div[@class="label"]/text()')
+    else:
+        print "Error!"
+        print "Maybe wrong username or password."
+        return
     for name, usage in zip(service_name, service_usage):
         print name
         print usage

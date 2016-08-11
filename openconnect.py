1fe107c844de93820aa0037c53d29b84eadc005d
moe <moevery@gmail.com>
2016-08-11 22:36:12 +0800

    add connection mode switching option

--- openconnect.py
+++ openconnect.py
@@ -6,8 +6,9 @@ from lxml import html
 from config import getConfig
 import os
 
-login_url = 'https://bluecloud.xyz/auth/login'
+login_url       = 'https://bluecloud.xyz/auth/login'
 server_list_url = 'https://bluecloud.xyz/server'
+template        = open('openconnect_tpl.sh').read()
 
 def sendRequest(form):
     session = requests.Session()
@@ -20,12 +21,11 @@ def sendRequest(form):
         os.mkdir('openconnect')
     for server, protocol in zip(server_link, protocol_support):
         if protocol.find(u'AnyConnect') != -1 and server.find('hk') != 0:
-            command = 'echo "{}" |  sudo openconnect --user={} --authgroup=smart --passwd-on-stdin {}:4433'\
-                .format(form['password'], form['identity'], server)
+            command = template.format(password=form['password'], user=form['identity'], server=server)
             filename = 'openconnect/{}.sh'.format(server)
             open(filename, 'w').write(command)
             os.chmod(filename, 0700)
-            print server, protocol
+            print server, "script generated!"
 
 def main():
     form = getConfig()
---
+++ openconnect_tpl.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+GROUP=$1
+MODE="smart"
+SERVER="{server}"
+PASSWD="{password}"
+USER="{user}"
+
+function start_connection
+{{
+	echo $PASSWD | sudo openconnect --user=$USER --authgroup=$MODE --passwd-on-stdin $SERVER:4433 
+}}
+
+function show_connection_info
+{{
+	echo `date`
+    echo "SERVER: $SERVER"
+	echo "  USER: $USER"
+}}
+
+function show_message
+{{
+
+	echo "==================================="
+	show_connection_info
+	# no group assigned
+	echo "-----------------------------------"
+
+	if [ $GROUP == "" ]; then
+		echo "RUN WITH GROUP NUMBER:"
+		echo "1) smart"
+		echo "2) global"
+		echo "3) smart-blacklist"
+	else
+		case $GROUP in
+			1)
+				MODE="smart"
+			;;
+			2)
+				MODE="global"
+			;;
+			3)
+				MODE="smart-blacklist"
+			;;
+			*)
+				MODE="smart"
+		esac
+		echo "RUNNING IN [$MODE] MODE"
+	fi
+	
+	echo "==================================="
+}}
+
+show_message
+start_connection

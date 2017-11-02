524f2316ed661f194ed2c8a65bd5d1d48c37737f
icymorn <moevery@gmail.com>
2017-11-02 20:16:33 +0800

    更新获取脚本

--- shadowsocks.py
+++ shadowsocks.py
@@ -23,21 +23,24 @@ def sendRequest(form):
     response = session.post(login_url, data=form)
     # response = session.get('https://monocloud.net/home')
     tree = html.fromstring(response.content)
+    print("login as:", form['email'])
     # import ipdb; ipdb.set_trace()
 
     myService = tree.xpath(u'//*[@id="sidebar-menu"]/ul')[0]
 
     shadowsocksPage = myService.xpath('//a[starts-with(@href, "https://monocloud.net/service/")]/@href')
-    for url in shadowsocksPage:
-        getShadowsocksInfo(session, url.replace('service', 'shadowsocks'))
+    shadowsocksPageName = myService.xpath('//a[starts-with(@href, "https://monocloud.net/service/")]/text()')
+
+    for url, pageName in zip(shadowsocksPage, shadowsocksPageName):
+        if 'Classic' not in pageName:
+            getShadowsocksInfo(session, url.replace('service', 'shadowsocks'))
 
 def getShadowsocksInfo(session, url):
+    print("get url: ", url)
     response = session.get(url)
     tree = html.fromstring(response.content)
     entries = tree.xpath('//table[@class="table"]')
     serviceName = tree.xpath('//*[@class="page-title"]/text()')[0]
-    if "Classic" in serviceName:
-        return
     print serviceName
     print "================="
     if not os.path.isdir(serviceName):
@@ -59,7 +62,7 @@ def generateFileWin(folder, entry):
 
 def generateFile(folder, entry):
     texts = entry.xpath('.//td/text()')
-    extra, server, port, ratio, password, method = texts
+    extra, server, port, ratio, password, method = texts[:6]
     command = template.format(server=server, method=method, password=password, port=port, ratio=ratio, extra=extra)
     with codecs.open(folder + '/' + server + '.sh', 'w', encoding='utf-8') as file:
         file.write(command)
